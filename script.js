3b3e5502c2d8164490632ac553cae1444872e560
Graham Theobald <Graham_Theobald@live.co.uk>
2022-10-07 20:14:42 +0100

    desktop view

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 /*
-.main__about__products__product
-.top__left-select
-.b
+add .selected to these when selected
+    .main__about__products__product
+    .top__left-select
+    .b
+
+add .bookmarked to .bookmark when clicked
 */
